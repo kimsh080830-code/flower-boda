07c3eb3f41de0ca896e68db07445ef5a3641c156
kimsh080830-code <kimsh080830@gmail.com>
2026-09-15 10:12:47 +0900

    image: add curated cornelian cherry reference photo

--- src/js/data.js
+++ src/js/data.js
@@ -4,6 +4,11 @@ const img = (id) => __flowerAssets[id] || 'assets/flowers/flower-fallback-01.web
 const pendingImg = 'data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%204%203%22%3E%3Crect%20width%3D%224%22%20height%3D%223%22%20fill%3D%22%23eceee9%22%2F%3E%3C%2Fsvg%3E';
 
 const REFERENCE_IMAGE_OVERRIDES = Object.freeze({
+  'cornelian-cherry': {
+    url: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Cornus_officinalis.JPG',
+    creator: 'Doctoroftcm', license: 'CC BY-SA 3.0',
+    sourceUrl: 'https://commons.wikimedia.org/wiki/File:Cornus_officinalis.JPG'
+  },
   sasanqua: {
     url: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Camellia_sasanqua.jpg',
     creator: 'Juni', license: 'CC BY 2.0',
