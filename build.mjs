998c77339926643e1ec36a11784c83ecc6cd07ad
kimsh080830-code <kimsh080830@gmail.com>
2026-09-17 14:00:24 +0900

    build: output DEV bundle as index.html

--- build.mjs
+++ build.mjs
@@ -6,7 +6,7 @@ const root=path.dirname(fileURLToPath(import.meta.url));
 const read=relative=>readFile(path.join(root,relative),'utf8');
 const order=JSON.parse(await read('src/modules.json'));
 const sourceModuleScripts='const __mods=Object.create(null);\n'+(await Promise.all(order.map(name=>read('src/'+name)))).join('\n');
-new vm.Script('\"use strict\";\n'+sourceModuleScripts);
+new vm.Script('"use strict";\n'+sourceModuleScripts);
 const imageManifest=JSON.parse(await read('data/image-assets.json'));
 const externalImageMetadata=JSON.parse(await read('data/external-image-metadata.json'));
 const auditReport=JSON.parse(await read('data/data-audit-v61.json'));
@@ -22,12 +22,12 @@ async function inlineLocalImages(source){
   return output;
 }
 const moduleScripts=await inlineLocalImages(sourceModuleScripts);
-new vm.Script('\"use strict\";\n'+moduleScripts);
+new vm.Script('"use strict";\n'+moduleScripts);
 const styles=await read('src/styles.css');
 const template=await read('src/index.template.html');
 const buildHtml=(dev)=>{
-  const scripts='\"use strict\";\nglobalThis.__FLOWER_APP_DEV__='+String(dev)+';\nglobalThis.__FLOWER_APP_DEV_PAYLOAD__='+(dev?devPayload:'null')+';\n'+moduleScripts;
+  const scripts='"use strict";\nglobalThis.__FLOWER_APP_DEV__='+String(dev)+';\nglobalThis.__FLOWER_APP_DEV_PAYLOAD__='+(dev?devPayload:'null')+';\n'+moduleScripts;
   return template.replace('/* APP_STYLES */',()=>styles).replace('/* APP_SCRIPTS */',()=>scripts.replace(/<\/script/gi,'<\\/script'));
 };
-await writeFile(path.join(root,'꽃을보다_V61_dev.html'),buildHtml(true));
-console.log('V61 dev build complete');
+await writeFile(path.join(root,'index.html'),buildHtml(true));
+console.log('V61 dev build complete: index.html');
